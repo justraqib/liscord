39acda79e67a722e20007a303fb342eb03e7e1d2
Raqib Ahmed <raqibahmed3822@gmail.com>
2022-08-27 11:36:30 +0530

    add channel view

--- app/views.py
+++ app/views.py
@@ -1,29 +1,53 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from app.models import Channel, Server
+from app.models import Channel, Message, Server
 
 
 @login_required
 def profile_index(request):
-    user = request.user
     template_name = "profiles/index.html"
     ctx = {
-        "user": user,
+        "user": request.user,
     }
     return render(request, template_name, ctx)
 
+@login_required
 def lobby_index(request):
     ctx = {
         "servers": Server.objects.all(),
+        "user": request.user,
     }
     template_name = "lobby/index.html"
     return render(request, template_name, ctx)
 
+@login_required
 def server_view(request, server_id):
+    selected_server = Server.objects.get(id=server_id)
     ctx = {
         "servers": Server.objects.all(),
-        "channels": Channel.objects.filter(server=server_id)
+        "selected_server": selected_server,
+        "channels": Channel.objects.filter(server=selected_server),
+        "user": request.user,
     }
     template_name = "lobby/server.html"
+    return render(request, template_name, ctx)
+
+@login_required
+def channel_view(request, channel_id):
+    if request.method == "POST":
+        message = request.POST.get("message")
+        Message.objects.create(message=message, channel_id=channel_id, created_by=request.user)
+
+    selected_channel = Channel.objects.get(id=channel_id)
+    selected_server = selected_channel.server
+    ctx = {
+        "servers": Server.objects.all(),
+        "selected_server": selected_server,
+        "channels": Channel.objects.filter(server=selected_server),
+        "selected_channel": selected_channel,
+        "messages": Message.objects.filter(channel=selected_channel),
+        "user": request.user,
+    }
+    template_name = "lobby/channel.html"
     return render(request, template_name, ctx)
