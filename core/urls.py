57c12d7cba9296f69d33a9f5c3c2b918a44ba5cc
Raqib Ahmed <justraqib@gmail.com>
2023-04-29 16:36:44 +0530

    Add avatar and setup static files

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("profiles/", include("apps.profiles.urls")),
+    path('avatar/', include('avatar.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
