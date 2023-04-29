e5124736996e2f1107c076ba714f5bf38e9a8503
Raqib Ahmed <justraqib@gmail.com>
2023-04-29 16:36:44 +0530

    add registration form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,24 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-from app.models import Server
+from app.models import Server, UserProfile
+
+
+class RegisterForm(UserCreationForm):
+    name = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ["name", "username", "password1", "password2"]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+            name = self.cleaned_data['name']
+            UserProfile.objects.create(user=user, name=name)
+        return user
 
 class ServerForm(forms.ModelForm):
     class Meta:
